fff98a60df595da27d76ec5aa26c9bdc6e050cbd
momin-mostafa <mominmostafa.g@gmail.com>
2021-08-25 01:08:49 +0600

    globals comment formatted.

--- lib/globals.dart
+++ lib/globals.dart
@@ -4,6 +4,6 @@ bool isDrawerOpen = false;
 
 // will be used for custom drawer listtile
 
-double totalMonthlyCost = 0; // init values will be 0
-double totalWeeklyCost = 0; // it will be updated when the app starts and reads
+double totalMonthlyCost = 0; // init values will be 0, it will be
+double totalWeeklyCost = 0; // updated when the app starts and reads
 double totalDailyCost = 0; // file stored in local storage.
