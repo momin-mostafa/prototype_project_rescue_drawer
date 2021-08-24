adec2614ae7ebcea28ed45756e62e0cc415a024b
momin-mostafa <mominmostafa.g@gmail.com>
2021-08-25 00:53:45 +0600

    globals comments added.

--- lib/globals.dart
+++ lib/globals.dart
@@ -2,6 +2,8 @@ library prototype_project_rescue_drawer.globals;
 
 bool isDrawerOpen = false;
 
-double totalMonthlyCost = 0;
-double totalWeeklyCost = 0;
-double totalDailyCost = 0;
+// will be used for custom drawer listtile
+
+double totalMonthlyCost = 0; // init values will be 0
+double totalWeeklyCost = 0; // it will be updated when the app starts and reads
+double totalDailyCost = 0; // file stored in local storage.
